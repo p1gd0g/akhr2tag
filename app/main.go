b84cae4e0d18bf924d45dc7cbb926fae61a30175
p1gd0g <renhao.szu@gmail.com>
2020-03-23 02:46:42 +0800

    add firebase

--- app/main.go
+++ app/main.go
@@ -161,6 +161,24 @@ func (h *hello) Render() app.UI {
 				app.Text(h.names[i]),
 			)
 		}),
+		app.Raw(`<script src="https://www.gstatic.com/firebasejs/7.12.0/firebase-app.js"></script>`),
+		app.Raw(`<script src="https://www.gstatic.com/firebasejs/7.12.0/firebase-analytics.js"></script>`),
+		app.Raw(`    <script>
+        // Your web app's Firebase configuration
+        var firebaseConfig = {
+            apiKey: "AIzaSyAtUtdV-7cUFbwmp3oq7rqNQi3AxjeT1-s",
+            authDomain: "quickstart-1554221638006.firebaseapp.com",
+            databaseURL: "https://quickstart-1554221638006.firebaseio.com",
+            projectId: "quickstart-1554221638006",
+            storageBucket: "quickstart-1554221638006.appspot.com",
+            messagingSenderId: "792826622257",
+            appId: "1:792826622257:web:f106c15b737d1dfb5d8010",
+            measurementId: "G-GVLCS2STV1"
+        };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+        firebase.analytics();
+    </script>`),
 	)
 }
 
